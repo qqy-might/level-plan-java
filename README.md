626aeb357049b06a59643cb8b756d6d8a5bc00a4
PC-202010150204\Administrator <545664719@qq.com>
2021-04-04 02:03:06 +0800

    first init : add mysql

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # level-plan-java
-level plan for java
+level 1 = 基本使用 + 基本原理
+
+level 2 = 优雅使用 + 关联关系
+
+level 3 = 深入原理 + 架构实现
+
+
+
+
+
+本次需要实现
+
+- [ ] mysql level 3
+- [ ] redis level 2
+- [ ] mq level 2
+- [ ] springboot level2
+- [ ] duboo level 1
---
+++ springboot-lr-mysql/src/main/java/com/example/qqy/springbootlrmysql/SpringbootLrMysqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.qqy.springbootlrmysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootLrMysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootLrMysqlApplication.class, args);
+	}
+
+}
